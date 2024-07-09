c8b019a61a321d12dad277cc20518647278fd3ca
Ashmi Sinha <162735046+ashmayyhh@users.noreply.github.com>
2024-07-10 01:00:50 +0530

    Update Footer.js

--- client/src/components/Layout/Footer.js
+++ client/src/components/Layout/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return (
         <div className="bg-dark text-light p-4">
-            <h6 className="text-center">All rights reserved &copy;KeerthanaArun</h6>
+            <h6 className="text-center">All rights reserved &copy;Cjo1dIej9GN3N-O6su3sP</h6>
         </div>
     )
 }
