6de89a15ffaa084670cae370a21d20662ae2aad0
Ashmi Sinha <162735046+ashmayyhh@users.noreply.github.com>
2024-07-10 01:27:19 +0530

    Update transactionRoutes.js

--- routes/transactionRoutes.js
+++ routes/transactionRoutes.js
@@ -2,18 +2,14 @@ const express = require('express');
 const { addTransaction, getAllTransaction, editTransaction, deleteTransaction } = require('../controllers/transactionCtrl');
 
 
-// router object
 const router = express.Router();
 
-//routes
-//add transaction
 router.post('/add-transaction', addTransaction);
 
 router.post('/edit-transaction', editTransaction);
 
 router.post('/delete-transaction', deleteTransaction);
 
-//get transaction
 router.get('/get-transaction', getAllTransaction);
 
 module.export = router;
