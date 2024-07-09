615c820ee949f3dcf5460ce54fdc3195d3479b57
keerthana_arun <130483493+keerthanaaarun357@users.noreply.github.com>
2024-07-09 13:37:04 +0530

    Modified server files

---
+++ routes/transactionRoutes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+
+// router object
+const router = express.Router();
+
+//routes
+//add transaction
+
+module.export = router;
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const morgan = require('morgan');
 const dotenv = require('dotenv');
 const colors = require('colors');
 const connectDb = require('./config/connectDb');
+const path = require("path");
 
 // config dot env file
 dotenv.config();
@@ -20,8 +21,16 @@ app.use(express.json());
 app.use(cors());
 
 // routes
-app.get('/', (req, res) => {
-    res.send("<h1>Hello from server</h1>");
+app.use('/api/v1/users', require("./routes/userRoute"))
+
+//transactions routes
+app.use("/api/v1/transactions", require("./routes/transactionRoutes"));
+
+//static files
+app.use(express.static(path.join(__dirname, "./client/build")));
+
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
 // port
