89709cdbec1381b47129d78718bf662befe17319
keerthana_arun <130483493+keerthanaaarun357@users.noreply.github.com>
2024-07-10 10:32:14 +0530

    Update HomePage.js handleDelete

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -46,7 +46,7 @@ const HomePage = () => {
                         setEditable(record)
                         setShowModal(true)
                     }} />
-                    <DeleteOutlined className="mx-2" />
+                    <DeleteOutlined className="mx-2" onClick={() => handleDelete(record)}/>
                 </div>
             )
         }
