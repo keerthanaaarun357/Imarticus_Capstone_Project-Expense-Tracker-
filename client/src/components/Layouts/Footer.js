50b7b18ec525d3d475cf1c36e6097bd286cf5612
keerthana_arun <130483493+keerthanaaarun357@users.noreply.github.com>
2024-07-11 15:23:11 +0530

    Update Footer.js

--- client/src/components/Layouts/Footer.js
+++ client/src/components/Layouts/Footer.js
@@ -4,7 +4,7 @@ import "./Footer.css"; // Custom CSS file
 const Footer = () => {
   return (
     <div className="custom-footer">
-      <h6 className="footer-text">All rights reserved &copy; KeerthanaArun</h6>
+      <h6 className="footer-text">All rights reserved &copy; Team ID : Cjo1dIej9GN3N-O6su3sP</h6>
     </div>
   );
 };
