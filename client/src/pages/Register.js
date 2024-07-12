18f0791ffcfde3b684c63137bb8e688fd7f7deff
keerthana_arun <130483493+keerthanaaarun357@users.noreply.github.com>
2024-07-12 18:21:16 +0530

    Update Register.js

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -14,7 +14,7 @@ const Register = () => {
         try {
             console.log(values);
             setLoading(true);
-            await axios.post("/users", values);
+            await axios.post("/api/v1/users/register", values);
             message.success("Registration Successful! Welcome aboard!");
             setLoading(false);
             navigate("/login");
